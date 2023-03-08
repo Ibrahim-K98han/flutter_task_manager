d0cad3e368bf476b5fac1d11dfe18c316229f8db
Ibrahim Khan <aitvetibrahim@gmail.com>
2023-03-08 23:30:01 +0600

    compile and terget sdk version set done

--- lib/ui/screen/update_profile_screen.dart
+++ lib/ui/screen/update_profile_screen.dart
@@ -5,6 +5,7 @@ import 'package:flutter_task_manager/ui/widgets/app_elevated_button.dart';
 import 'package:flutter_task_manager/ui/widgets/app_text_field_widget.dart';
 import 'package:flutter_task_manager/ui/widgets/screen_background_widget.dart';
 import 'package:flutter_task_manager/ui/widgets/user_profile_widget.dart';
+import 'package:image_picker/image_picker.dart';
 
 class UpdateProfileScreen extends StatefulWidget {
   const UpdateProfileScreen({Key? key}) : super(key: key);
@@ -14,12 +15,13 @@ class UpdateProfileScreen extends StatefulWidget {
 }
 
 class _UpdateProfileScreenState extends State<UpdateProfileScreen> {
-
   final TextEditingController _emailETController = TextEditingController();
   final TextEditingController _firstNameETController = TextEditingController();
   final TextEditingController _lastNameETController = TextEditingController();
   final TextEditingController _mobileETController = TextEditingController();
   final TextEditingController _passwordETController = TextEditingController();
+
+  XFile? pickedImage;
 
   @override
   void initState() {
@@ -27,7 +29,7 @@ class _UpdateProfileScreenState extends State<UpdateProfileScreen> {
     _emailETController.text = AuthUtils.email ?? '';
     _firstNameETController.text = AuthUtils.firstName ?? '';
     _lastNameETController.text = AuthUtils.lastName ?? '';
-     _mobileETController.text = AuthUtils.mobile ?? '';
+    _mobileETController.text = AuthUtils.mobile ?? '';
   }
 
   @override
@@ -56,21 +58,26 @@ class _UpdateProfileScreenState extends State<UpdateProfileScreen> {
                           height: 16,
                         ),
                         InkWell(
-                          onTap: (){
-
+                          onTap: () async {
+                            pickedImage = await ImagePicker()
+                                .pickImage(source: ImageSource.gallery);
+                            if (pickedImage != null) {
+                              // setState(() {});
+                            }
                           },
                           child: Row(
                             children: [
                               Container(
                                 padding: const EdgeInsets.all(16),
                                 decoration: const BoxDecoration(
-                                  color: Colors.grey,
-                                  borderRadius: BorderRadius.only(
-                                    topLeft: Radius.circular(8),
-                                    bottomLeft: Radius.circular(8)
-                                  )
+                                    color: Colors.grey,
+                                    borderRadius: BorderRadius.only(
+                                        topLeft: Radius.circular(8),
+                                        bottomLeft: Radius.circular(8))),
+                                child: const Text(
+                                  'Photo',
+                                  style: TextStyle(color: Colors.white),
                                 ),
-                                child: const Text('Photo',style: TextStyle(color: Colors.white),),
                               ),
                               Expanded(
                                 child: Container(
@@ -79,10 +86,8 @@ class _UpdateProfileScreenState extends State<UpdateProfileScreen> {
                                       color: Colors.white,
                                       borderRadius: BorderRadius.only(
                                           topRight: Radius.circular(8),
-                                          bottomRight: Radius.circular(8)
-                                      )
-                                  ),
-                                  child: const Text(''),
+                                          bottomRight: Radius.circular(8))),
+                                  child: Text(pickedImage?.name ?? ''),
                                 ),
                               ),
                             ],
@@ -92,38 +97,42 @@ class _UpdateProfileScreenState extends State<UpdateProfileScreen> {
                           height: 8,
                         ),
                         AppTextFieldWidget(
-                            hintText: 'Email', controller: _emailETController,
-                            readOnly: true,
+                          hintText: 'Email',
+                          controller: _emailETController,
+                          readOnly: true,
                         ),
                         const SizedBox(
                           height: 8,
                         ),
                         AppTextFieldWidget(
-                            hintText: 'First Name', controller: _firstNameETController
+                            hintText: 'First Name',
+                            controller: _firstNameETController),
+                        const SizedBox(
+                          height: 8,
+                        ),
+                        AppTextFieldWidget(
+                            hintText: 'Last Name',
+                            controller: _lastNameETController),
+                        const SizedBox(
+                          height: 8,
+                        ),
+                        AppTextFieldWidget(
+                          hintText: 'Mobile ',
+                          controller: _mobileETController,
                         ),
                         const SizedBox(
                           height: 8,
                         ),
                         AppTextFieldWidget(
-                            hintText: 'Last Name', controller: _lastNameETController
-                        ),
-                        const SizedBox(
-                          height: 8,
-                        ),
-                        AppTextFieldWidget(
-                            hintText: 'Mobile ', controller: _mobileETController,
-                        ),
-                        const SizedBox(
-                          height: 8,
-                        ),
-                        AppTextFieldWidget(hintText: 'Password',
+                            hintText: 'Password',
                             obscureText: true,
-                            controller: _passwordETController
-                        ),
+                            controller: _passwordETController),
                         const SizedBox(
                           height: 16,
                         ),
-                        AppElevatedButton(child: Icon(Icons.arrow_circle_right_outlined), onTap: (){})
+                        AppElevatedButton(
+                            child: Icon(Icons.arrow_circle_right_outlined),
+                            onTap: () {})
                       ],
                     ),
                   ),
