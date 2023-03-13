462f5fcb4142b8bbb0c5b5b592f73fe77f6284d7
Ibrahim Khan <aitvetibrahim@gmail.com>
2023-03-13 22:51:27 +0600

    splash screen done

--- lib/task_manager2/ui/screen/splash_screen_2.dart
+++ lib/task_manager2/ui/screen/splash_screen_2.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class SplashScreen2 extends StatefulWidget {
   const SplashScreen2({Key? key}) : super(key: key);
@@ -10,6 +11,25 @@ class SplashScreen2 extends StatefulWidget {
 class _SplashScreen2State extends State<SplashScreen2> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    final Size screenSize = MediaQuery.of(context).size;
+    return Scaffold(
+      body: Stack(
+        children: [
+          SvgPicture.asset(
+            'assets/images/background.svg',
+            width: screenSize.width,
+            height: screenSize.height,
+            fit: BoxFit.cover,
+          ),
+          Center(
+              child: SvgPicture.asset(
+                  'assets/images/logo.svg',
+                width: 150,
+                fit: BoxFit.scaleDown,
+              )
+          )
+        ],
+      ),
+    );
   }
 }
